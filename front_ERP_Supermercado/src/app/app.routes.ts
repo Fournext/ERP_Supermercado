468808de243dfcbd14733ea392aada774f053d5a
Fournext <sebastianzc04@gmail.com>
2025-05-08 00:21:27 -0400

    arreglo de errores

--- front_ERP_Supermercado/src/app/app.routes.ts
+++ front_ERP_Supermercado/src/app/app.routes.ts
@@ -78,6 +78,14 @@ export const routes: Routes = [
         loadComponent:()=>import('./business/boleta-salida/boleta-salida.component')
       },
       {
+        path:'proveedor',
+        loadComponent:()=>import('./business/proveedores/proveedores.component')
+      },
+      {
+        path:'compras',
+        loadComponent:()=>import('./business/compras-page/compras-page.component')
+      },
+      {
         path: '',
         redirectTo: 'dashboard',
         pathMatch: 'full'
