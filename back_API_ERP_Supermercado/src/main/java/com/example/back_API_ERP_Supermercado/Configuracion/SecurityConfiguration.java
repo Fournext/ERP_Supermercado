b29a71dcb33bf0114bf2ebab32fe7b10a0782542
Fournext <sebastianzc04@gmail.com>
2025-08-11 01:42:27 -0400

    cloudfare

--- back_API_ERP_Supermercado/src/main/java/com/example/back_API_ERP_Supermercado/Configuracion/SecurityConfiguration.java
+++ back_API_ERP_Supermercado/src/main/java/com/example/back_API_ERP_Supermercado/Configuracion/SecurityConfiguration.java
@@ -48,8 +48,7 @@ public class SecurityConfiguration {
                 "http://localhost:3000",  // Frontend React/Vue
                 "http://localhost:4200",
                 "http://localhost:4000",// Frontend Angular
-                "https://tudominio.com",
-                "http://ec2-18-117-180-26.us-east-2.compute.amazonaws.com:4000"
+                "https://servidor.fournext.me"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
--- front_ERP_Supermercado/src/environments/environment.development.ts
+++ front_ERP_Supermercado/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     //endpoint: 'http://localhost:3005/api/'
-    endpoint: 'http://ec2-18-117-180-26.us-east-2.compute.amazonaws.com:3005/api/'
+    endpoint: 'https://back.fournext.me/api/'
+    //endpoint: 'http://ec2-18-117-180-26.us-east-2.compute.amazonaws.com:3005/api/'
 };
